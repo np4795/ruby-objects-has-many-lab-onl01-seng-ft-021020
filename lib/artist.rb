f8098ee264d2c94387975bc2aff44919859c023b
Nicholas Patel <np4795@yahoo.com>
2020-02-25 23:21:36 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,5 @@
 class Artist
-
-  attr_accessor :name, :songs
+  attr_accessor :name
 
   @@song_count = 0
 
@@ -12,18 +11,21 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
-    @@song_count += 1
+    @@song_count +=2
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
-    @songs << song
+    self.songs << song
     song.artist = self
-    @@song_count += 1
+    @@song_count +=1
+  end
+
+  def songs
+     Song.all.select {|song| song.artist == self}
   end
 
   def self.song_count
     @@song_count
   end
-
-end
+en
