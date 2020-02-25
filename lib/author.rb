976e82ea693c651cbc4f25b2f5dcb3226fef471f
Nicholas Patel <np4795@yahoo.com>
2020-02-25 23:36:30 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,31 +1,24 @@
 class Author
   attr_accessor :name
 
-  @@post_count = 0
-
   def initialize(name)
     @name = name
-    @posts = []
-  end
-
-  def add_post(post)
-    @posts << post
-    post.author = self
-    @@post_count +=1
-  end
-
-  def add_post_by_title(title)
-    post = Post.new(title)
-    self.posts << post
-    post.author = self
-    @@post_count +=2
   end
 
   def posts
-    Post.all.select {|post| post.author == self}
+    Post.all.select {|p| p.author == self}
+  end
+
+  def add_post(post)
+    post.author = self
+  end
+
+  def add_post_by_title(post_title)
+    post = Post.new(post_title)
+    add_post(post)
   end
 
   def self.post_count
-    @@post_count
+    Post.all.length
   end
 end
